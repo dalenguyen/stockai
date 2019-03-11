b57545323b7d62e825a3fb3c99cf858c7bcafbc6
Dale Nguyen <dungnq@itbox4vn.com>
2019-03-11 07:54:04 -0400

    Added Stock class

--- test/test.py
+++ test/test.py
@@ -1,3 +1,13 @@
-import stockai
 
-print(stockai.name)
+import sys
+
+if sys.version_info < (2, 7):
+    from unittest2 import main as test_main, SkipTest, TestCase
+else:
+    from unittest import main as test_main, SkipTest, TestCase
+
+from stockai.settings import Values
+from stockai import Base
+
+test = Base._prepare_request()
+print(test)
