a704cf567e1b59eeaf734dfa1ee5207be1eebcd6
Dale Nguyen <dnguyen@ci.com>
2019-03-12 17:37:28 -0400

    Added dependencies

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="stockai",
-    version="0.0.3",
+    version="0.0.4",
     author="Dale Nguyen",
     author_email="dungnq@itbox4vn.com",
     description="Get stock info from Yahoo! Finance",
@@ -13,6 +13,7 @@ setuptools.setup(
     long_description_content_type="text/markdown",
     url="https://github.com/dalenguyen/stockai",
     packages=setuptools.find_packages(),
+    install_requires = [ 'ciso8601', 'requests', ],
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
--- stockai/__init__.py
+++ stockai/__init__.py
@@ -2,7 +2,7 @@
 Stockai - get stock information from Yahoo! Finance
 """
 
-__version__ = '0.0.3'
+__version__ = '0.0.4'
 __author__ = 'Dale Nguyen'
 __name__ = 'stockai'
 
