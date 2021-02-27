e6de1e0be1a2442fcb83b443c60ef5499a14ea43
Dale Nguyen <dungnq@itbox4vn.com>
2021-02-27 13:57:02 -0500

    Added setup guide

--- setup.py
+++ setup.py
@@ -3,6 +3,12 @@ import setuptools
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
+# Deploy to PyPI
+#
+# pip install twine
+# python setup.py sdist bdist_wheel
+# twine upload --skip-existing dist/*
+
 setuptools.setup(
     name="stockai",
     version="1.0.0",
@@ -13,7 +19,7 @@ setuptools.setup(
     long_description_content_type="text/markdown",
     url="https://github.com/dalenguyen/stockai",
     packages=setuptools.find_packages(),
-    install_requires = [ 'ciso8601', 'requests', ],
+    install_requires = [ 'ciso8601', 'requests', 'loguru' ],
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
